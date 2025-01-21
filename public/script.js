514b3691ab74405c8002234426d81abef318a6e0
AmirKhaleghi08 <khaleghi.am84@gmail.com>
2025-01-21 23:17:37 +0330

    mvp launch

---
+++ public/script.js
@@ -0,0 +1,50 @@
+const FundraiseSubmenuBtn = document.querySelector("#fundraise-btn");
+const FundraiseSubmenu = document.querySelector("#fundraise-content");
+const ThemChanger = document.querySelector("#them-changer");
+
+FundraiseSubmenuBtn.addEventListener("click", (event) => {
+    event.preventDefault()
+    FundraiseSubmenu.classList.toggle("hidden")
+    FundraiseSubmenuBtn.classList.toggle("rotate-180")
+})
+
+document.addEventListener("DOMContentLoaded", (event) => {
+   
+if (document.documentElement.classList.contains("dark")) {
+    ThemChanger.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+</svg>                  
+                <span>تم روشن</span>
+`
+ console.log("hii") 
+} else{
+    ThemChanger.innerHTML =  `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+  </svg>                  
+<span>تم تاریک</span>`
+
+}
+})
+
+ThemChanger.addEventListener("click", (event) => {
+    event.preventDefault()
+   
+if (document.documentElement.classList.contains("dark")) {
+    document.documentElement.classList.remove("dark")
+    ThemChanger.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+                  </svg>                  
+                <span>تم تاریک</span>`
+localStorage.theme = "light";
+
+} else{
+    document.documentElement.classList.add("dark")
+    ThemChanger.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+</svg>                  
+                <span>تم روشن</span>
+`
+localStorage.setItem("theme" , "dark");
+
+}
+})
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  darkMode: `class`,
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
